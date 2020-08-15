f50f21db6d28ae8e56ebb51b665cd5da0f8e9fd6
ipavlopoulos <annis@aueb.gr>
2020-08-15 19:06:44 +0300

    Added random baseline and updated InputErasure with a get_offsets method.

--- baselines/models.py
+++ baselines/models.py
@@ -1,16 +1,54 @@
 import pandas as pd
+import random
+
+
+class Random:
+
+    def __init__(self, text):
+        """
+        Random baseline for toxic span detection.
+        Returns toxic offsets at random.
+        :param text:
+        """
+        self.toxic_offsets = [i for i, ch in enumerate(text) if random.random()>0.5]
+
+    def get_toxic_offsets(self):
+        return self.toxic_offsets
 
 
 class InputErasure:
 
-    def __init__(self, classifier, text, one_by_one=False, class_names=[0, 1], mask=u"[mask]", threshold=0.2, reshape_predictions=True):
+    def __init__(self,
+                 classifier,
+                 text,
+                 one_by_one=False,
+                 tokenise=lambda txt: txt.split(),
+                 class_names=[0, 1],
+                 mask=u"[mask]",
+                 threshold=0.2,
+                 reshape_predictions=True):
+        """
+        Given a classifier and a tokenisation method InputErasure returns the toxic words and respective offsets.
+        This implementation is based on the paper "Understanding Neural Networks through Representation Erasure" by
+        Li et al.
+
+        :param classifier: any toxicity classifier that predicts a text as toxic or not
+        :param text: the textual input (sentence or document) as a string
+        :param one_by_one: some classifiers may require one by one classification when scoring the "ablated" texts.
+        :param tokenise: by default splits the words on empty space
+        :param class_names: by default "toxic" is represented by 1 and "civil" by 0
+        :param mask: the pseudo token to mask the toxic word (for visualisation purposes)
+        :param threshold: above this value the text is predicted toxic (default 0.2)
+        :param reshape_predictions: flattens the output, some classifiers may required this to be set to False
+        """
         self.class_names = class_names
         self.classifier = classifier
         self.mask = mask
         self.one_by_one = one_by_one
         self.reshape_predictions = reshape_predictions
         self.initial_score = self.clf_predict([text])
-        self.words = self.classifier.tokenise(text)
+        self.tokenise = tokenise
+        self.words = self.tokenise(text)
         self.ablations, self.indices = self.create_ablations()
         self.scores = self.clf_predict(self.ablations)
         self.e = 10e-05
@@ -40,6 +78,20 @@ class InputErasure:
 
     def get_black_list(self):
         return [self.indices[i] for i, s in enumerate(self.scores_decrease) if s > self.threshold]
+
+    def get_toxic_offsets(self):
+        """
+        Get the offsets of the toxic spans
+        WARNING: Valid only for empty space tokenisation.
+        :return: a list with offsets found toxic
+        """
+        current_offset = 0
+        toxic_offsets = []
+        for i, word in enumerate(self.words):
+            if i in set(self.black_list):
+                toxic_offsets.extend(list(range(current_offset, len(word))))
+            current_offset += len(word) + 1
+        return toxic_offsets
 
     def get_mitigated_text(self):
         return " ".join([w if i not in set(self.black_list) else self.mask for i, w in enumerate(self.words)])
